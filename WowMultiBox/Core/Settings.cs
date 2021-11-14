6f297212f4efbb41d599a53f9a32b8bd74dea182
David <David@c0degeek.dev>
2021-11-14 22:35:38 +0100

    Initial create + launch

---
+++ WowMultiBox/Core/Settings.cs
@@ -0,0 +1,24 @@
+﻿namespace WowMultiBox.Core;
+
+public class Settings
+{
+    public string GamePath { get; set; }
+    public string GameExe { get; set; }
+    public string GameName { get; set; }
+    public List<string> CharacterNames { get; set; }
+
+    public string FullPath => $"{GamePath}{GameExe}";
+    
+    public int SlaveHeight { get; set; }
+    public int SlaveWidth { get; set; }
+    public int Slave1PosX { get; set; }
+    public int Slave1PosY { get; set; }
+    public int SlaveFocusHeight { get; set; }
+    public int SlaveFocusWidth { get; set; }
+    public int SlaveFocusPosX { get; set; }
+    public int SlaveFocusPosY { get; set; }
+    public int MasterPosX { get; set; }
+    public int MasterPosY { get; set; }
+    public int MasterWidth { get; set; }
+    public int MasterHeight { get; set; }
+}
---
+++ WowMultiBox/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using WowMultiBox.Core;
+
+namespace WowMultiBox;
+
+static class Program
+{
+    private static Settings _settings = null!;
+
+    private static readonly Dictionary<string, Process> RunningClients = new();
+
+    public static void Main(string[] args)
+    {
+        _settings = GetSettings() ?? throw new Exception("Must specify settings");
+
+        RunProcesses();
+    }
+
+    private static void RunProcesses()
+    {
+        var isFirst = true;
+        var slavePosIndex = 0;
+        foreach (var characterName in _settings.CharacterNames)
+        {
+            var process = new Process();
+            
+            process.StartInfo.FileName = _settings.FullPath;
+            process.StartInfo.Arguments = $"-config Config-{characterName}.WTF";
+
+            process.Start();
+            while (process.MainWindowHandle == IntPtr.Zero)
+                Thread.Sleep(10);
+            User32.SetWindowText(process.MainWindowHandle, characterName);
+
+            if (!RunningClients.TryAdd(characterName, process))
+                throw new Exception($"Failed to add process {characterName} to running list");
+
+            // Master
+            if (isFirst)
+            {
+                User32.MoveWindow(process.MainWindowHandle, _settings.MasterPosX, _settings.MasterPosY,
+                    _settings.MasterWidth, _settings.MasterHeight, false);
+
+                isFirst = false;
+                continue;
+            }
+
+            var slavePosY = _settings.Slave1PosY + slavePosIndex * _settings.SlaveHeight;
+            
+            User32.MoveWindow(process.MainWindowHandle, _settings.Slave1PosX, slavePosY,
+                _settings.SlaveWidth, _settings.SlaveHeight, false);
+
+            slavePosIndex++;
+        }
+    }
+
+    private static Settings? GetSettings()
+    {
+        var text = File.ReadAllText("settings.json");
+        return JsonSerializer.Deserialize<Settings>(text);
+    }
+}
+
